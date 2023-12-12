1b91189637356bf4c4df89e0571bed4f481469b6
Agata-M-S <agata.webdevelopment@gmail.com>
2023-12-12 10:12:21 +0000

    add date to the articles

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,10 +3,21 @@ import { LiaComments } from "react-icons/lia";
 
 export const ArticleCard = ({ article }) => {
 
+function formatDate(dateString){
+  const options = { year: 'numeric', month: 'short', day: 'numeric' };
+  return new Date(dateString).toLocaleDateString([],options);
+}
+
+
 	return (
 		<>
 			<img src={article.article_img_url} />
-			<h4>{article.title}</h4>
+			<h4>
+				{article.title}
+				<br />
+			<span>	{formatDate(article.created_at)} </span>
+			</h4>
+
 			<p>
 				By:
 				{article.author}
