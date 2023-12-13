824c7c96acd73b1856a84d578ef15f4af8e2df75
Agata-M-S <agata.webdevelopment@gmail.com>
2023-12-13 12:04:23 +0000

    add catch block to the comments and change styling for votes

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -7,19 +7,25 @@ import {} from "react-icons/bi";
 export const Comments = ({ article_id }) => {
 	const [comments, setComments] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+  const [errorClass, setErrorClass] = useState('hidden')
 
 	useEffect(() => {
+    setErrorClass('hidden')
 		setIsLoading(true);
 		getCommentsByArticleId(article_id).then((data) => {
 			setComments(data.comments);
 			setIsLoading(false);
-		});
+		}).catch(()=>{
+      setIsLoading(false)
+      setErrorClass('show')
+    });
 	}, []);
 	if (isLoading) {
 		return <section className="loading-screen">results are loading</section>;
 	}
 	return (
 		<ul>
+      <p className={errorClass}>Can't load comments</p>
 			{comments.map((comment) => {
 				return (
 					<li key={comment.comment_id}>
