6bfd2288fb606cbff90a24f902ac6833678a73c2
Agata-M-S <agata.webdevelopment@gmail.com>
2023-12-14 15:18:59 +0000

    fix ability to duplicate comments on slow connection

--- src/components/CommentAdder.jsx
+++ src/components/CommentAdder.jsx
@@ -8,9 +8,11 @@ export const CommentAdder = ({ article_id, setComments }) => {
 	const [input, setInput] = useState("");
 	const { user } = useContext(UserContext);
 	const [apiErr, setApiErr] = useState(null);
+  const [isDisabled, setIsDisabled] =useState(false)
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
+    
 		if (!user) {
 			console.log("no user");
 			Promise.reject({
@@ -21,6 +23,7 @@ export const CommentAdder = ({ article_id, setComments }) => {
 			console.log("no input");
 			Promise.reject({ status: 400, msg: "'you can't post an empty comment" });
 		} else {
+      setIsDisabled(true)
 			postComment(article_id, user, input)
 				.then((res) => {
 					setInput("");
@@ -31,6 +34,7 @@ export const CommentAdder = ({ article_id, setComments }) => {
 							created_at: res.comment.created_at,
 							votes: res.comment.votes,
 						};
+            setIsDisabled(false)
 						return [newComment, ...currComments];
 					});
 				})
@@ -56,14 +60,15 @@ export const CommentAdder = ({ article_id, setComments }) => {
 					/>
 					{apiErr ? <Error message={apiErr} /> : null}
 					<br />
-					<button
-						onClick={() => {
+					<button disabled={isDisabled}
+						onClick={(e) => {
 							if (!user) {
 								setApiErr("you need to be logged in to post a comment");
 							} else if (!input) {
 								setApiErr("you can't post an empty comment");
 								console.log("no input");
 							}
+              
 						}}
 						onBlur={() => {
 							setApiErr(null);
